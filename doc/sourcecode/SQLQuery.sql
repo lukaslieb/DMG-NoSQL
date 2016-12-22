47719b555950ec6f9bec14abe4dec5bbf71f071d
lukaslieb <lukaslieb@hotmail.ch>
2016-12-22 19:36:46 +0100

    fix sqlquery, and builing in texstudio

---
+++ doc/sourcecode/SQLQuery.sql
@@ -0,0 +1,21 @@
+select ProfessorName, AnzahlStudenten, SummeSWS 
+from ( 
+	select
+		p.Name as ProfessorName, 
+        count(s.MatrNr) as AnzahlStudenten 
+	from Professoren p 
+	join Vorlesungen v on v.gelesenVon = p.PersNr
+	join hoeren h on h.VorlNr = v.VorlNr 
+	join Studenten s on s.MatrNr = h.MatrNr 
+	group by p.Name 
+) A 
+join 
+( 
+	select 
+		p.Name as ProfessorName, 
+        sum(SWS) as summeSWS 
+	from Professoren p 
+	join Vorlesungen v on v.gelesenVon = p.PersNr 
+	group by p.Name 
+) B 
+using(ProfessorName);
